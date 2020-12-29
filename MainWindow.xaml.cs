4bd44a9fbce46bdf8e96d2322a239b1a01d8a86c
Nikita28007 <Nikita.galeev@iesdoctorbalmis.com>
2020-12-29 20:22:34 +0100

    examinarButtun

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,38 @@ namespace Proyecto_DINT
             InitializeComponent();
         }
 
-    
+        private void cargaJSONButton(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                openFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+                openFileDialog.InitialDirectory = @"c:\Users\%USERNAME%\Documents";
+            }
+        }
+
+        private void guardaJSONButton(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+
+                saveFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+                saveFileDialog.InitialDirectory = @"c:\Users\%USERNAME%\Documents";
+            }
+        }
+        private void examinarButtton(object sender, RoutedEventArgs e)
+        {
+           
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                openFileDialog.Filter = "jpg files (*.jpg)|*.jpg|png files (*.png)|*.png|JPEG files (*.jpeg)|*.jpeg|All files (*.*)|*.*";
+                openFileDialog.InitialDirectory = @"c:\Users\%USERNAME%\Documents";
+                imagenPeli.Text = openFileDialog.FileName;
+
+            }
+
+        }
     }
 }
