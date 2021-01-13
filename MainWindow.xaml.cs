441b62739239c9b3627fb4c900fc024bbee3c014
Nikita28007 <Nikita.galeev@iesdoctorbalmis.com>
2021-01-13 21:59:51 +0100

    verPelis

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,7 +32,15 @@ namespace Proyecto_DINT
             listaPelicula = Peliculas.listaPeliculas();
             peliDockpanel.DataContext = listaPelicula.FirstOrDefault();
             actualTextBlock.Text = "1";
+            Pelis.ItemsSource = listaPelicula;
+            Console.WriteLine(listaPelicula[0].Nombre);
+       
+        }
 
+        private void cargarImagenes()
+        {
+            ObservableCollection<Peliculas> oc;
+          
         }
 
         private void cargaJSONButton(object sender, RoutedEventArgs e)
@@ -40,7 +49,7 @@ namespace Proyecto_DINT
             if (openFileDialog.ShowDialog() == true)
             {
                 openFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
-                openFileDialog.InitialDirectory = @"c:\Users\%USERNAME%\Documents";
+                openFileDialog.InitialDirectory = @"C:\Users\%USERNAME%\Documents";
             }
         }
 
@@ -51,7 +60,7 @@ namespace Proyecto_DINT
             {
 
                 saveFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
-                saveFileDialog.InitialDirectory = @"c:\Users\%USERNAME%\Documents";
+                saveFileDialog.InitialDirectory = @"C:\Users\%USERNAME%\Documents";
             }
         }
         private void examinarButtton(object sender, RoutedEventArgs e)
@@ -61,7 +70,7 @@ namespace Proyecto_DINT
             if (openFileDialog.ShowDialog() == true)
             {
                 openFileDialog.Filter = "jpg files (*.jpg)|*.jpg|png files (*.png)|*.png|JPEG files (*.jpeg)|*.jpeg|All files (*.*)|*.*";
-                openFileDialog.InitialDirectory = @"c:\Users\%USERNAME%\Documents";
+                openFileDialog.InitialDirectory = @"C:\Users\%USERNAME%\Documents";
                 imagenPeli.Text = openFileDialog.FileName;
 
             }
@@ -81,6 +90,63 @@ namespace Proyecto_DINT
             }
         }
 
+        private void rightImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int actual = Int32.Parse(actualTextBlock.Text);
 
+            if (actual > 1)
+            {
+                peliDockpanel.DataContext = listaPelicula[actual + 2];
+                actualTextBlock.Text = (actual - 1).ToString();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string nivel="";
+           
+            if (TituloPelicula!= null && pista!= null && imagenPeli!= null && (facilRadioButton.IsChecked == true|| normalRadioButton.IsChecked ==true||dificilRadioButton.IsChecked == true)&&GeneroComboBox.SelectedIndex>-1)
+            {
+                if (facilRadioButton.IsChecked==true)
+                {
+                    nivel = "Facil";
+                }
+              else if (normalRadioButton.IsChecked == true)  nivel = "normal"; 
+              else if (dificilRadioButton.IsChecked == true)  nivel = "dificil"; 
+                Peliculas pelicula = new Peliculas(imagenPeli.Text,TituloPelicula.Text,pista.Text,nivel,GeneroComboBox.Text);
+                
+                listaPelicula.Add(pelicula);
+
+                MessageBox.Show("pelicula añadida");
+            }
+            else
+            {
+                MessageBox.Show("revisa que se ha introducido todos los datos");
+            }
+            Pelis.Items.Refresh();
+        }
+
+        private void Pelis_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void data_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+      
+            Eliminar.IsEnabled = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Peliculas peli = (Peliculas)Pelis.SelectedItem;
+            if (peli!=null)
+            { 
+                listaPelicula.Remove((Peliculas)Pelis.SelectedItem);
+                Pelis.Items.Remove(peli);
+               
+            }
+        
+        }
     }
 }
--- peliculas.cs
+++ peliculas.cs
@@ -99,13 +99,13 @@ namespace Proyecto_DINT
             List<Peliculas> listaPeli = new List<Peliculas>();
 
             Peliculas peli1 = new Peliculas("https://i.pinimg.com/originals/b1/5c/84/b15c848ec8f0e0fead75062e8fa09b00.jpg", "Leon", "asesino con la flor", "facil", "Acción/drama");
-            Peliculas peli2 = new Peliculas("https://oren-m7.ru/images/300/DSC100358454.jpg", "inception", "6 personas que tienen los mismos sueños", "medio", "Acción/Ciencia ficción");
-            Peliculas peli3 = new Peliculas("https://image.tmdb.org/t/p/original/kKTPv9LKKs5L3oO1y5FNObxAPWI.jpg", "erase una vez en holywood", "victimas: Sharon tate", "dificil", "Comedia/Comedia dramática");
+            Peliculas peli2 = new Peliculas("https://oren-m7.ru/images/300/DSC100358454.jpg", "Inception", "6 personas que tienen los mismos sueños", "medio", "Acción/Ciencia ficción");
+            Peliculas peli3 = new Peliculas("https://image.tmdb.org/t/p/original/kKTPv9LKKs5L3oO1y5FNObxAPWI.jpg", "Erase una vez en holywood", "victima: Sharon tate", "dificil", "Comedia/Comedia dramática");
             
             listaPeli.Add(peli1);
             listaPeli.Add(peli2);
             listaPeli.Add(peli3);
-
+           
             return listaPeli;
 
         }
