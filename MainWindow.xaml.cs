3f1d9360f49aef833e0d93ae3aa191a4f5cefbe7
Nikita28007 <Nikita.galeev@iesdoctorbalmis.com>
2021-01-15 22:10:44 +0100

    final

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,12 +19,17 @@ using System.Windows.Shapes;
 
 namespace Proyecto_DINT
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-
+        int puntuacion = 0;
+        int peliAdivinada = 4;
+        int generoDificil = 3;
+        int generoMedio = 2;
+        int generoFacil = 1;
+        int contador = 0;
+        private Peliculas peliculaActiva = new Peliculas();
+        private List<Peliculas> p = new List<Peliculas>();
         private List<Peliculas> listaPelicula;
 
         public MainWindow()
@@ -33,17 +38,105 @@ namespace Proyecto_DINT
             InitializeComponent();
             listaPelicula = Peliculas.listaPeliculas();
             peliDockpanel.DataContext = listaPelicula.FirstOrDefault();
-            actualTextBlock.Text = "1";
             Pelis.ItemsSource = listaPelicula;
-            Console.WriteLine(listaPelicula[0].Nombre);
-       
+            if (listaPelicula.Count >= 5)
+            {
+                PeliculasAleatoria();
+            }
+
+
+
         }
 
-        private void cargarImagenes()
+        private void fijarImagen(int index)
         {
-            ObservableCollection<Peliculas> oc;
-          
+            string imagen;
+            imagen = p[index].Imagen;
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(imagen, UriKind.RelativeOrAbsolute);
+            bitmapImage.EndInit();
+            JuegoImage.Source = bitmapImage;
+            BitmapImage generos = new BitmapImage();
+           
+            switch (peliculaActiva.Genero)
+            {
+                case "accion":
+                    generos.BeginInit();
+                    generos.UriSource = new Uri("../generos/acción.jpg", UriKind.RelativeOrAbsolute);
+                    generos.EndInit();
+                    generoImage.Source = generos;
+                    break;
+
+                case "Comedia":
+                    generos.BeginInit();
+                    generos.UriSource = new Uri("../generos/comedia.jpg", UriKind.RelativeOrAbsolute);
+                    generos.EndInit();
+                    generoImage.Source = generos;
+                    break;
+
+                case "Drama":
+                    generos.BeginInit();
+                    generos.UriSource = new Uri("../generos/drama.jpg", UriKind.RelativeOrAbsolute);
+                    generos.EndInit();
+                    generoImage.Source = generos;
+                    break;
+                case "Terror":
+                    generos.BeginInit();
+                    generos.UriSource = new Uri("../generos/terror.jpg", UriKind.RelativeOrAbsolute);
+                    generos.EndInit();
+                    generoImage.Source = generos;
+                    break;
+                case "Ficcion":
+                    generos.BeginInit();
+                    generos.UriSource = new Uri("../generos/ficción.jpg", UriKind.RelativeOrAbsolute);
+                    generos.EndInit();
+                    generoImage.Source = generos;
+                    break;
+                case "Ciencia":
+                    generos.BeginInit();
+                    generos.UriSource = new Uri("../generos/Ciencia.jpg", UriKind.RelativeOrAbsolute);
+                    generos.EndInit();
+                    generoImage.Source = generos;
+                    break;
+
+                default:
+                    break;
+            }
+
+            switch (peliculaActiva.Nivel)
+            {
+                case "dificil":
+                    mainGrid.Background = Brushes.DarkRed;
+                    break;
+                case "medio":
+                    mainGrid.Background = Brushes.DarkGoldenrod;
+                    break;
+                case "facil":
+                    mainGrid.Background = Brushes.DarkGreen;
+                    break;
+                default:
+                    break;
+            }
+
         }
+
+        
+        private void PeliculasAleatoria()
+        {
+            int peliRandom;
+
+
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                peliRandom = rnd.Next(0, listaPelicula.Count);
+                p.Add(listaPelicula[peliRandom]);
+            }
+            peliculaActiva = p[0];
+            fijarImagen(0);
+        }
+
 
         private void cargaJSONButton(object sender, RoutedEventArgs e)
         {
@@ -59,20 +152,19 @@ namespace Proyecto_DINT
                     using (StreamReader r = new StreamReader(openFileDialog.FileName))
                     {
                         string json = r.ReadToEnd();
-                        Peliculas items = JsonConvert.DeserializeObject<Peliculas>(json);
-                       
+                        listaPelicula = JsonConvert.DeserializeObject<List<Peliculas>>(json);
+                        Console.WriteLine("Hola mundo" + listaPelicula[3].Nombre);
                     }
-                    
-
-
-
-
                 }
             }
+            Pelis.ItemsSource = listaPelicula;
+            Pelis.Items.Refresh();
         }
 
         private void guardaJSONButton(object sender, RoutedEventArgs e)
         {
+
+
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             if (saveFileDialog.ShowDialog() == true)
             {
@@ -80,7 +172,7 @@ namespace Proyecto_DINT
                 saveFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
                 saveFileDialog.InitialDirectory = @"C:\Users\%USERNAME%\Documents";
 
-                string output = JsonConvert.SerializeObject(Pelis);
+                string output = JsonConvert.SerializeObject(listaPelicula);
                 try
                 {
                     string name = saveFileDialog.FileName;
@@ -91,9 +183,6 @@ namespace Proyecto_DINT
                 {
                     MessageBox.Show(ex.Message);
                 }
-
-
-
 
             }
         }
@@ -106,7 +195,6 @@ namespace Proyecto_DINT
                 openFileDialog.Filter = "jpg files (*.jpg)|*.jpg|png files (*.png)|*.png|JPEG files (*.jpeg)|*.jpeg|All files (*.*)|*.*";
                 openFileDialog.InitialDirectory = @"C:\Users\%USERNAME%\Documents";
                 imagenPeli.Text = openFileDialog.FileName;
-
             }
 
         }
@@ -115,40 +203,47 @@ namespace Proyecto_DINT
 
         private void leftImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            int actual = Int32.Parse(actualTextBlock.Text);
 
-            if (actual > 1)
+
+            if (contador >= 1 && p.Count >= 4)
             {
-                peliDockpanel.DataContext = listaPelicula[actual - 2];
-                actualTextBlock.Text = (actual - 1).ToString();
+                contador--;
+                peliculaActiva = p[contador];
+                fijarImagen(contador);
+
             }
+            actualTextBlock.Text = (contador + 1).ToString();
         }
 
         private void rightImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            int actual = Int32.Parse(actualTextBlock.Text);
 
-            if (actual > 1)
+
+            if (contador < 4 && p.Count >= 4)
             {
-                peliDockpanel.DataContext = listaPelicula[actual + 2];
-                actualTextBlock.Text = (actual - 1).ToString();
+                contador++;
+                peliculaActiva = p[contador];
+
+                fijarImagen(contador);
+
             }
+            maximoTextBlock.Text = (contador + 1).ToString();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void anyadirButton(object sender, RoutedEventArgs e)
         {
-            string nivel="";
-           
-            if (TituloPelicula!= null && pista!= null && imagenPeli!= null && (facilRadioButton.IsChecked == true|| normalRadioButton.IsChecked ==true||dificilRadioButton.IsChecked == true)&&GeneroComboBox.SelectedIndex>-1)
+            string nivel = "";
+
+            if (TituloPelicula != null && pista != null && imagenPeli != null && (facilRadioButton.IsChecked == true || normalRadioButton.IsChecked == true || dificilRadioButton.IsChecked == true) && GeneroComboBox.SelectedIndex > -1)
             {
-                if (facilRadioButton.IsChecked==true)
+                if (facilRadioButton.IsChecked == true)
                 {
                     nivel = "Facil";
                 }
-              else if (normalRadioButton.IsChecked == true)  nivel = "normal"; 
-              else if (dificilRadioButton.IsChecked == true)  nivel = "dificil"; 
-                Peliculas pelicula = new Peliculas(imagenPeli.Text,TituloPelicula.Text,pista.Text,nivel,GeneroComboBox.Text);
-                
+                else if (normalRadioButton.IsChecked == true) nivel = "normal";
+                else if (dificilRadioButton.IsChecked == true) nivel = "dificil";
+                Peliculas pelicula = new Peliculas(imagenPeli.Text, TituloPelicula.Text, pista.Text, nivel, GeneroComboBox.Text);
+
                 listaPelicula.Add(pelicula);
 
                 MessageBox.Show("pelicula añadida");
@@ -160,27 +255,77 @@ namespace Proyecto_DINT
             Pelis.Items.Refresh();
         }
 
-        private void Pelis_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void habilitarBoton(object sender, SelectionChangedEventArgs e)
         {
-            
-        }
 
-        private void data_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-      
             Eliminar.IsEnabled = true;
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Eliminar_Click(object sender, RoutedEventArgs e)
         {
+            bool selccionado = true;
+
+            Pelis.SelectedItem = selccionado;
             Peliculas peli = (Peliculas)Pelis.SelectedItem;
-            if (peli!=null)
-            { 
+            if (peli != null)
+            {
                 listaPelicula.Remove((Peliculas)Pelis.SelectedItem);
-                Pelis.Items.Remove(peli);
-               
+
+
             }
-        
+            Pelis.Items.Refresh();
+        }
+
+        private void validarButton(object sender, RoutedEventArgs e)
+        {
+            if (TituloTextBox.Text.Equals(peliculaActiva.Nombre))
+            {
+
+                if (verPistaCheckBox.IsChecked == true)
+                {
+                    peliAdivinada = peliAdivinada / 2;
+                    puntuacion += peliAdivinada;
+                }
+                else
+                {
+                    puntuacion += peliAdivinada;
+                }
+
+                if (peliculaActiva.Nivel.Equals("dificil"))
+                {
+                    puntuacion += generoDificil;
+                }
+                else if (peliculaActiva.Nivel.Equals("medio")) { puntuacion += generoMedio; }
+                else if (peliculaActiva.Nivel.Equals("facil")) { puntuacion += generoFacil; }
+                PuntuacionLabel.Content = puntuacion;
+
+
+                TituloTextBox.Text = "";
+            }
+            else
+            {
+                TituloTextBox.Text = "Titulo incorrecto";
+            }
+
+        }
+
+        private void nuevaPartidaButton_click(object sender, RoutedEventArgs e)
+        {
+            if (listaPelicula.Count >= 5)
+            {
+                PeliculasAleatoria();
+
+            }
+            else
+            {
+                MessageBox.Show("Debe haber al menos 5 peliculas");
+            }
+
+        }
+
+        private void verPistaCheckBox_checked(object sender, RoutedEventArgs e)
+        {
+            textoPistaTextBlock.Text = peliculaActiva.Pista;
         }
     }
 }
--- peliculas.cs
+++ peliculas.cs
@@ -84,7 +84,9 @@ namespace Proyecto_DINT
                 }
             }
         }
-
+        public Peliculas()
+        {
+        }
         public Peliculas(string imagen, string nombre, string pista, string nivel, string genero)
         {
             Imagen = imagen;
@@ -98,13 +100,18 @@ namespace Proyecto_DINT
         {
             List<Peliculas> listaPeli = new List<Peliculas>();
 
-            Peliculas peli1 = new Peliculas("https://i.pinimg.com/originals/b1/5c/84/b15c848ec8f0e0fead75062e8fa09b00.jpg", "Leon", "asesino con la flor", "facil", "Acción/drama");
-            Peliculas peli2 = new Peliculas("https://oren-m7.ru/images/300/DSC100358454.jpg", "Inception", "6 personas que tienen los mismos sueños", "medio", "Acción/Ciencia ficción");
-            Peliculas peli3 = new Peliculas("https://image.tmdb.org/t/p/original/kKTPv9LKKs5L3oO1y5FNObxAPWI.jpg", "Erase una vez en holywood", "victima: Sharon tate", "dificil", "Comedia/Comedia dramática");
+            Peliculas peli1 = new Peliculas("https://i.pinimg.com/originals/b1/5c/84/b15c848ec8f0e0fead75062e8fa09b00.jpg", "Leon", "asesino con la flor", "facil", "Drama");
+            Peliculas peli2 = new Peliculas("https://oren-m7.ru/images/300/DSC100358454.jpg", "Inception", "6 personas que tienen los mismos sueños", "medio", "Acción");
+            Peliculas peli3 = new Peliculas("https://image.tmdb.org/t/p/original/kKTPv9LKKs5L3oO1y5FNObxAPWI.jpg", "Erase una vez en holywood", "victima: Sharon tate", "dificil", "Comedia");
+            Peliculas peli4 = new Peliculas("https://as01.epimg.net/epik/imagenes/2020/05/06/portada/1588761236_799202_1588761314_noticia_normal_recorte1.jpg", "Gladiator", "Maximus", "medio", "Acción");
+            Peliculas peli5 = new Peliculas("https://www.tonica.la/__export/1587957633788/sites/debate/img/2020/04/26/avengers-endgame-momentos-epicos-no-podemos-dejar-ver_1.jpg_423682103.jpg", "Los Vengadores", "thor,iron man,hulk,thanos", "facil", "ficción");
+           
             
             listaPeli.Add(peli1);
             listaPeli.Add(peli2);
             listaPeli.Add(peli3);
+            listaPeli.Add(peli4);
+            listaPeli.Add(peli5);
            
             return listaPeli;
 
